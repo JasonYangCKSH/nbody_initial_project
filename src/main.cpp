0b58a0ccf758422804d7d7b0fab258a8e3cde1b2
Jason Yang <jasonyangcksh@gmail.com>
2026-03-01 20:46:42 +0800

    first main design

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <random>
+#include <cmath>
+#include <chrono>
 #include <glm/glm.hpp>
 #include "simulation.h"
 #include "body.h"
 
 int main() {
-    // 1. Initialize Simulation Parameters
-    // dt = 0.01s, theta = 0.5 (accuracy), epsilon = 0.1 (softening)
+
     float dt = 0.01f;
     float theta = 0.5f;
     float epsilon = 0.1f;
-    std::ofstream fout("output.csv");
+
     Simulation sim(dt, theta, epsilon);
 
-    // 2. Create some initial bodies
-    // Example: Two bodies attracting each other
-    Body sun(
-        glm::vec3(0.0f, 0.0f, 0.0f),    // Position
-        glm::vec3(0.0f, 0.0f, 0.0f),    // Velocity
-        glm::vec3(0.0f, 0.0f, 0.0f),    // Acceleration
-        1000.0f,                        // Mass
-        1.0f                            // Radius
-    );
+    std::mt19937 gen(42);
+    std::uniform_real_distribution<float> disPos(-50.0f, 50.0f);
+    size_t bodyNum = 10000;
+    for (size_t i = 0; i < bodyNum; i++) {
 
-    Body planet(
-        glm::vec3(10.0f, 0.0f, 0.0f),   // Position
-        glm::vec3(0.0f, 5.0f, 0.0f),    // Velocity (Orbiting speed)
-        glm::vec3(0.0f, 0.0f, 0.0f),    // Acceleration
-        1.0f,                           // Mass
-        0.2f                            // Radius
-    );
-
-    sim.bodies.push_back(sun);
-    sim.bodies.push_back(planet);
-
-    std::cout << "Starting Simulation with " << sim.bodies.size() << " bodies..." << std::endl;
-    std::cout << "----------------------------------------------" << std::endl;
-
-    // 3. Run Simulation Loop
-    int total_steps = 1000000;
-    for (int i = 0; i < total_steps; ++i) {
-        sim.step();
-        for (size_t b = 0; b < sim.bodies.size(); b++) {
-            glm::vec3 position = sim.bodies[b].pos;
-            fout << position.x << " " << position.y << " " << position.z << "\t"; 
-        } 
-        fout << "\n";
-        /*
-        // Print progress every 10 steps
-        if (i % 10 == 0) {
-            std::cout << "Frame: " << sim.frame << std::endl;
-            for (size_t b = 0; b < sim.bodies.size(); ++b) {
-                const auto& p = sim.bodies[b].pos;
-                std::cout << "  Body " << b << " Pos: (" << p.x << ", " << p.y << ", " << p.z << ")" << std::endl;
-            }
-            std::cout << "-----------------------" << std::endl;
-        }*/
+        Body b;
+        b.pos = glm::vec3(disPos(gen), disPos(gen), disPos(gen));
+        b.vel = glm::vec3(0.0f); 
+        b.mass = 5.0f;
+        b.radius = 0.2f;
+        sim.bodies.push_back(b);
     }
-    fout.close();
-    std::cout << "Simulation finished." << std::endl;
+
+    int totalStep = 100;
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < totalStep; i++) {
+        //std::cout << "Processing Step: " << i + 1 << std::endl;
+        sim.step();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> time = end - start;
+    std::cout << "Time Spend: " << time.count()<< std::endl;
 
     return 0;
 }
