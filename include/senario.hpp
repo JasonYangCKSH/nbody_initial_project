c8f74d4d9860cc396bba8caaaf35ac706c476eec
JasonYangCKSH <jason@DESKTOP-EE8PSSA.localdomain>
2026-03-22 13:03:31 +0800

    update

---
+++ include/senario.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+#include <random>
+#include "body.hpp"
+namespace Senario {
+    // 1. uniform distributed bodies
+    std::vector<Body> UniformRandom() {
+    }
+};
