03152d292ecfd1384e5c4ba59cf13e024a4e0ef3
JasonYangCKSH <jason@DESKTOP-EE8PSSA.localdomain>
2026-03-22 16:44:40 +0800

    update

--- include/NeighborSearchOctree.hpp
+++ include/NeighborSearchOctree.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #include "Barnes-HutOctree.hpp"  // only referring Oct(boundary)
 #include <vector>
+
 class NeighborSearchNode {
 private:
-    Oct boundary;
+    Oct boundary;  // use Oct to represent AABB
     int firstChild;
     int nextSibling;
+    std::vector<NeighborSearchNode> neighborNodesVector;
 public:
     NeighborSearchNode(): boundary(), firstChild(0), nextSibling(0){}
     bool isLeaf() const {return (firstChild == 0);}
