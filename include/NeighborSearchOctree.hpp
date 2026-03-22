9fe0658f4592702dfaa852a9bdc055d1ee5abe9a
JasonYangCKSH <jason@DESKTOP-EE8PSSA.localdomain>
2026-03-22 17:06:06 +0800

    update

--- include/NeighborSearchOctree.hpp
+++ include/NeighborSearchOctree.hpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include "body.hpp"
 
-class NeighborSearchNode {
+class NSNode {
 private:
     // -both of these are used to define the boundary of a node-
     glm::vec3 aabb_min;  
@@ -13,17 +13,17 @@ private:
     std::vector<int> bodiesIndicesVector;
     static constexpr int MAX_LEAF_CAPACITY = 8;  
 public:
-    NeighborSearchNode():  firstChild(0), nextSibling(0){}
+    NSNode():  firstChild(0), nextSibling(0){}
     bool isLeaf() const {return (firstChild == 0);}
     bool isEmpty() const {return bodiesIndicesVector.empty();}
 };
 
 
 
-class NeighborSearchOctree {
+class NSOctree {
 private:
-    std::vector<NeighborSearchNode> NSnodes;
-    std::vector<int> NSparents;
+    std::vector<NSNode> nsNodes;
+    std::vector<int> nsParents;
     std::vector<glm::vec3> positions;
     static const int ROOT = 0;
     void Insert(int node_idx, int body_idx) {
@@ -42,7 +42,14 @@ private:
 public:
 
     void Build(const std::vector<Body>& bodies) {
-
+        // step1: catch all position
+        position.clear();
+        for (Body& body : bodies) 
+            position.push_vack(b.pos);
+        // step2: build root node, aabb will wrap up all the particles
+        nsNodes.clear();
+        nsParent.clear();
+        
     }
 
     void Query(int idx, float r, std::vector<int>& out) const {
