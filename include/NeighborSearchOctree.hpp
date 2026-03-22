afb760e61fac351e47c3432a2047a1004c6ac4d5
JasonYangCKSH <jason@DESKTOP-EE8PSSA.localdomain>
2026-03-22 15:48:15 +0800

    update

--- include/NeighborSearchOctree.hpp
+++ include/NeighborSearchOctree.hpp
@@ -1,9 +1,23 @@
 #pragma once
 #include "Barnes-hutOctree.hpp"  // only referring Oct(boundary)
+#include <vector>
+class NeighborSearchNode {
+private:
+    Oct boundary;
+    
+public:
+
+};
+
+
+
 class NeighBorSearchOctree {
+private:
+    std::vector<NeighborSearchNode> NSnodes;
+     
 public:
 
 
 
-private:
+
 };
