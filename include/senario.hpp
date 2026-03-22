68a97b92285f3e89abe32efc53a4afda7e1e8caf
JasonYangCKSH <jason@DESKTOP-EE8PSSA.localdomain>
2026-03-22 13:13:51 +0800

    update

--- include/senario.hpp
+++ include/senario.hpp
@@ -4,6 +4,19 @@
 #include "body.hpp"
 namespace Senario {
     // 1. uniform distributed bodies
-    std::vector<Body> UniformRandom() {
+    std::vector<Body> UniformRandom(int N, float range, float mass, float radius) {
+        std::vector<Body> bodies;
+        body.reserve(N);  // 預留N筆空間
+
+        std::mt19937 rng(42);
+        std::uniform_real_distribution<float> dist(-range, range);
+    
+        for (int i = 0; i < N; i++) {
+            glm::vec3 pos = {dist(rng), dist(rng), dist(rng)};
+            glm::vec3 vel = {0, 0, 0};
+            glm::vec3 acc = {0, 0, 0};
+            bodies.push_back(Body(pos, vel, acc, mass, radius));
+        }
+        return bodies;
     }
 };
