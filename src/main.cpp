b6f4d497d28bede0b711da502149e4a4d0cc5595
JasonYangCKSH <jason@DESKTOP-EE8PSSA.localdomain>
2026-03-22 13:30:13 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <chrono>
 #include <glm/glm.hpp>
 #include "body.hpp"
 #include "Barnes-HutOctree.hpp"
 #include "simulation.hpp"
-using namespace std;
+auto now() {
+    return std::chrono::high_resolution_clock::now();
+}
+double ms(double start, double end) {
+    return std::chrono::duration<double, std::milli>(end - start).count();
+}
 int main() {
     // position range: ([-100, 100], [-100, 100], [-100, 100])
     // Simulation sim(0.01, 0.0, 0.1, NeighborMethod::BRUTE_FORCE, 0.1);
@@ -12,6 +18,12 @@ int main() {
     float epsilon = 0.1f;
     NeighborMethod neighbor_method = NeighborMethod::BRUTE_FORCE;
     std::vector<Body> bodies;
+
+
+
+
+
+
     Simulation sim(dt, theta, epsilon, bodies, neighbor_method);
     return 0;
 }
