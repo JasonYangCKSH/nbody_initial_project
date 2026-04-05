7920a703dff67982f9a573ee0eb479bd6c29a33f
JasonYangCKSH <jason@DESKTOP-EE8PSSA.localdomain>
2026-04-05 18:07:44 +0800

    upadate

--- soa/include/Barnes-hutOctree.hpp
+++ soa/include/Barnes-hutOctree.hpp
@@ -9,6 +9,28 @@ public:
     glm::vec3 center;
     float size;
     Oct new_containing(const BodySystem& bs) {
+        size_t n = bs.size();
+        if (n == 0) return *this;
 
+        float min_x = bs.posX[0], max_x = bs.posX[0];
+        float min_y = bs.posY[0], max_y = bs.posY[0];
+        float min_z = bs.posZ[0], max_z = bs.posZ[0];
+
+        // 單次掃描所有維度，最大化快取利用率
+        for (size_t i = 1; i < n; ++i) {
+            if (bs.posX[i] < min_x) min_x = bs.posX[i];
+            else if (bs.posX[i] > max_x) max_x = bs.posX[i];
+
+            if (bs.posY[i] < min_y) min_y = bs.posY[i];
+            else if (bs.posY[i] > max_y) max_y = bs.posY[i];
+
+            if (bs.posZ[i] < min_z) min_z = bs.posZ[i];
+            else if (bs.posZ[i] > max_z) max_z = bs.posZ[i];
+        }
+
+        center = glm::vec3(min_x + max_x, min_y + max_y, min_z + max_z) * 0.5f;
+        size = std::max({max_x - min_x, max_y - min_y, max_z - min_z});
+
+        return *this;
     }
 };
