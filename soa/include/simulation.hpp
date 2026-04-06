3f142fd8ace07f58e9f05a88f96ce86e2db865f5
JasonYangCKSH <jason@DESKTOP-EE8PSSA.localdomain>
2026-04-06 12:46:42 +0800

    update

--- soa/include/simulation.hpp
+++ soa/include/simulation.hpp
@@ -33,6 +33,13 @@ private:
         for (int i = 0; i < bs.posX.size(); i++) {
             octree.insert(bs, i);
         }
+        octree.propagate();
+        for (int i = 0; i < bs.posX.size(); i++) {
+            glm::vec3 acc = octree.calculate_acc(bs.posX[i], bs.posY[i], bs.posZ[i]);
+            bs.accX[i] = acc.x;
+            bs.accY[i] = acc.y;
+            bs.accZ[i] = acc.z;
+        }        
 
     }
     void collide() {
