e42aa22122b7baacf5cefefdc24c6cc559e4561d
JasonYangCKSH <jason@DESKTOP-EE8PSSA.localdomain>
2026-04-06 12:09:04 +0800

    update

--- soa/include/simulation.hpp
+++ soa/include/simulation.hpp
@@ -3,11 +3,12 @@
 #include <iostream>
 #include <glm/glm.hpp>
 #include <utility>
-
+#include "body_system.hpp"
+#include "Barnes-HutOctree.hpp"
 class Simulation {
 public:
     float dt;
     int frame;
-    
+    BodySystem bs;
 private:
 };
