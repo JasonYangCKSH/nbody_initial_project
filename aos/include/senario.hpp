481e67643a94315560b60ee7b0ad519a2fd1a030
Jason Yang <jasonyangcksh@gmail.com>
2026-04-06 22:26:38 +0800

    update

--- aos/include/senario.hpp
+++ aos/include/senario.hpp
@@ -80,9 +80,6 @@ namespace Senario {
         //1
         bodies.push_back(Body(glm::vec3(-1.0f, 0, 0), glm::vec3( 0.5f, 0, 0), glm::vec3(0), 1.0f, 1.0f));
         bodies.push_back(Body(glm::vec3( 1.0f, 0, 0), glm::vec3(-0.5f, 0, 0), glm::vec3(0), 1.0f, 1.0f));
-
-        
-        
         return bodies;
     }
 };
