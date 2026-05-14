40385172a19197faf14dcd963ffb265e68107fc0
Jason Yang <jasonyangcksh@gmail.com>
2026-05-14 14:43:44 +0800

    update

--- baseline/src/main.cpp
+++ baseline/src/main.cpp
@@ -5,13 +5,20 @@
 int main() {
     Simulation sim(1.0f); // searchRadius = 1.0 = 2 * body_radius, which is something to do with body's radius(0.5)
     sim.GenerateSimple();
+    // case1: brute force
     auto start = std::chrono::high_resolution_clock::now();
     auto pairs = sim.BruteForce();
     auto end = std::chrono::high_resolution_clock::now();
-
     sim.PrintPairsResult(pairs);
-
     std::chrono::duration<double, std::milli> elapsed = end - start;
     std::cout << "Time Spend: " << elapsed.count() << " ms\n";
+    
+    // case2: uniform grid
+
+
+
+    // case3: octree
+    
+    
     return 0;
 }
