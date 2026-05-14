8dd08ff06d26b27141dab13167df8662aecfbc96
Jason Yang <jasonyangcksh@gmail.com>
2026-05-14 14:38:21 +0800

    update

--- baseline/src/main.cpp
+++ baseline/src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "simulation.hpp"
 int main() {
-    Simulation sim(1.0f); // searchRadius = 1.0
+    Simulation sim(1.0f); // searchRadius = 1.0 = 2 * body_radius, which is something to do with body's radius(0.5)
     sim.GenerateSimple();
     auto pairs = sim.BruteForce();
     sim.PrintPairsResult(pairs);
