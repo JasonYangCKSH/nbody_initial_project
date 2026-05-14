979f62953f63846523d7d675cb668590edf83bc2
Jason Yang <jasonyangcksh@gmail.com>
2026-05-14 13:41:54 +0800

    update

---
+++ baseline/include/simulation.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <utility>
+#include <unordered_map>
+#include "body.hpp"
+class Simulation {
+private:
+
+
+public:
+
+
+};
