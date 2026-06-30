c2a5c98c2e97bb5ff6192ce04f34a623784b3715
JasonYangCKSH <jasonyangcksh@gmail.com>
2026-06-30 16:10:09 +0800

    updarte

---
+++ benchmark/include/point_set.h
@@ -0,0 +1,12 @@
+# pragma once
+#include <vector>
+#include <array>
+
+class PointSet {
+private:
+    std::vector<std::array<float, 3>> points;
+public:
+    PointSet(){}
+
+    void SetPointSet();
+};
