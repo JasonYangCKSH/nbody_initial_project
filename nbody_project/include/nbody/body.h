39e46d49b76399ec96b0a58c4fb12d5ee19956b2
JasonYangCKSH <jasonyangcksh@gmail.com>
2026-07-26 13:16:39 +0800

    update

--- nbody_project/include/nbody/body.h
+++ nbody_project/include/nbody/body.h
@@ -1,8 +1,20 @@
 #pragma once
-struct Body {
+#include "vec3.h"
+class Body {
 private:
-
+    int id;
 public:
+    
+    double mass;
+    double radius;
+    
+    Vec3 position;
+    Vec3 velocity;
+    Vec3 acceleration;
 
+    Body(){}
+    Body(int _id, double _mass, double _radius, Vec3 _position, Vec3 _velocity, Vec3 _acceleration):
+    id(_id), mass(_mass), radius(_radius), position(_position), velocity(_velocity), acceleration(_acceleration){}
 
+    int getID() const {return this->id;};
 };
