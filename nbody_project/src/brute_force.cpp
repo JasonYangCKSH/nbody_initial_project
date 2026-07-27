d5ceb1ecef5fa01d5123d7ae0ab8fe98d6591fe1
JasonYangCKSH <jasonyangcksh@gmail.com>
2026-07-27 21:39:10 +0800

    update

--- nbody_project/src/brute_force.cpp
+++ nbody_project/src/brute_force.cpp
@@ -11,18 +11,22 @@ void BruteForceCalculator::computeAccelerations(std::vector<Body>& bodies) {
         for (size_t j = i + 1; j < bodies.size(); ++j) {
 
             // 步驟3：算出 r = bodies[j].position - bodies[i].position
+            Vec3 r = bodies[j].position - bodies[i].position;
 
             // 步驟4：算出 distSqr = r.dot(r) + softening*softening
-
+            double distSqrt = r.dot(r) + softening * softening;
+        
             // 步驟5：算出 invDist3 = 1.0 / (distSqr * sqrt(distSqr))
+            double invDist3 = 1.0 / (distSqr * std::sqrt(distSqr));
 
             // 步驟6：更新 bodies[i].acceleration
             //         用公式：a_i += G * m_j * invDist3 * r
             //         （回想上一則訊息的公式4，分母質量是對方的m_j）
-
+            bodies[i].acceleration += r * (G * bodies[j].mass * invDist3);
+        
             // 步驟7：更新 bodies[j].acceleration
             //         方向相反，用牛頓第三定律：a_j += G * m_i * invDist3 * (-r)
-
+            bodies[j].acceleration += r * (-G * bodies[i].mass * invDist3);
         }
     }
 }
