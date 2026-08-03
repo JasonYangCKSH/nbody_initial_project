b1879ae6cdb545ff304c60c0bbfddf67df663231
JasonYangCKSH <jasonyangcksh@gmail.com>
2026-08-03 12:12:02 +0800

    update

--- nbody_project/include/nbody/octree.h
+++ nbody_project/include/nbody/octree.h
@@ -22,10 +22,38 @@ public:
 
     // TODO: insert(const Body& b)
     // 把一個粒子插入這棵樹（遞迴邏輯）
+    void insert(const Body& b) {
+        // case 1: empty leaf node
+        if (this->totalMass == 0.0) {
+            this->body = &b;
+            return;
+        }
+        // case 2: nonempty leaf node
+        if (this->isLeaf) {
+            this->isLeaf = false;
+            const Body *oldBody = this->body;
+            this->body = nullptr;
+            int oldIdx = getOctantIndex(oldBody->position);
+            
+            if (!children[oldIdx]) {
+                children[oldIdx] = std::make_unique<OctreeNode>(childCenter(oldIdx), halfSize * 0.5);
+            }
+            children[oldIdx]->insert(*oldBody);
+        }
+        // case 3: internal node
 
+    }
     // TODO: 一個輔助函式，判斷某個位置屬於8個子節點中的哪一個
     // 通常回傳 0~7 的index
+    int getOctantIndex(const Vec3& position) const {
+        int index = 0;
+        if (position.x >= center.x) index += 1;
+        if (position.y >= center.y) index += 2;
+        if (position.z >= center.z) index += 4;
+        
 
+        return index;
+    }
     // TODO: computeMassDistribution()
     // Bottom-up：遞迴計算每個節點的totalMass跟centerOfMass
 };
