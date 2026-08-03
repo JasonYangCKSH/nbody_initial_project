bb7117880276117514973fcc21a5cb6d3f65df6c
JasonYangCKSH <jasonyangcksh@gmail.com>
2026-08-03 11:44:16 +0800

    update

--- nbody_project/include/nbody/barnes_hut.h
+++ nbody_project/include/nbody/barnes_hut.h
@@ -1,1 +1,26 @@
 
+#pragma once
+#include "nbody/force_calculator.h"
+#include "nbody/octree.h"
+#include <memory>
+
+class BarnesHutCalculator : public ForceCalculator {
+public:
+    explicit BarnesHutCalculator(double G = 1.0, double softening = 0.0, double theta = 0.5)
+        : G_(G), softening_(softening), theta_(theta) {}
+
+    void computeAccelerations(std::vector<Body>& bodies) override;
+
+    void setTheta(double theta) { theta_ = theta; }
+
+private:
+    double G_;
+    double softening_;
+    double theta_;
+
+    // TODO: 建樹用的輔助函式
+    // std::unique_ptr<OctreeNode> buildTree(const std::vector<Body>& bodies);
+
+    // TODO: 對單一粒子，遞迴walk整棵樹算加速度
+    // Vec3 computeForceOnBody(const Body& target, const OctreeNode& node);
+};
--- nbody_project/include/nbody/octree.h
+++ nbody_project/include/nbody/octree.h
@@ -19,4 +19,13 @@ public:
     OctreeNode(const Vec3& _center, double _halfSize)
         : center(_center), halfSize(_halfSize) {}
 
+
+    // TODO: insert(const Body& b)
+    // 把一個粒子插入這棵樹（遞迴邏輯）
+
+    // TODO: 一個輔助函式，判斷某個位置屬於8個子節點中的哪一個
+    // 通常回傳 0~7 的index
+
+    // TODO: computeMassDistribution()
+    // Bottom-up：遞迴計算每個節點的totalMass跟centerOfMass
 };
