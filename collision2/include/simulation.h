4c8f2f8846c77776b994b1b9db3564d6564f2c10
JasonYangCKSH <jasonyangcksh@gmail.com>
2026-08-30 08:02:31 +0800

    update

--- collision2/include/simulation.h
+++ collision2/include/simulation.h
@@ -20,6 +20,13 @@ struct StepStats {
     int broadPhasePairs = 0;
     int collisionCount = 0;
     // TODO: 你覺得還需要記錄哪些？(對應你之前想的CSV欄位)
+    bool rebuild;
+};
+struct SimConfig {
+    float dt = 0.001f;
+    float K = 200.0f;
+    float cellSize = 1.0f;
+
 };
 enum class StructureMode {
     BruteForce,
@@ -34,6 +41,7 @@ private:
     std::vector<Particle> particles_;
 
     StructureMode mode_;
+    SimConfig cfg_;
     std::unique_ptr<std::variant<broad::UniformGrid, broad::Octree>> structure_;  // 只有UniformGrid/Octree用得到,brute force時可以是nullptr
     bool skinEnabled_;
 
